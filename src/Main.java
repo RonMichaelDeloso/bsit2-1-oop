efb5cb52f4347e3444e45afc0aae5899a5a8729b
RonMichaelDeloso <rdeloso12002@liceo.edu.ph>
2025-08-01 11:42:20 +0800

    lab-activity-2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import  java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        Scanner scanner = new Scanner(System.in);
+        int totalnumber = 0;
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("enter number: " + i + ": ");
+            int number = scanner.nextInt();
+            totalnumber += number;
+        }
+        System.out.println("total number: " + totalnumber);
     }
 }
