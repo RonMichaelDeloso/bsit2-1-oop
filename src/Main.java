b08ff9ecd3981afdd11f00d8bca1348be3236aa8
RonMichaelDeloso <rdeloso12002@liceo.edu.ph>
2025-08-05 11:54:28 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
+        String studentid = "20242283837";
+        String firstbaname = "Ron";
+        String lastname = "Deloso";
+        String course = "BSIT";
+        String section = "2-1";
+        int midterm = 89;
+        int finalexam = 87;
+        int ProjectS = 90;
+        int Attendance = 90;
+
+        int Total = midterm + finalexam + ProjectS + Attendance;
+        int AveScore = Total/4;
+
+        System.out.println("Student Name: " + firstbaname +lastname);
+        System.out.println("Student ID: " + studentid);
+        System.out.println("Student Name: " + firstbaname +lastname);
+        System.out.println("Course and section: " + course + section);
+        System.out.println(AveScore);
+        if (AveScore >= 75) {
+            System.out.println("PASSED!!!!");
+        }
+        else{
+            System.out.println("FAILED");
+        }
+
     }
 }
