61dda48d210f5f020f36a396360e23b078ce5656
RonMichaelDeloso <rdeloso12002@liceo.edu.ph>
2025-08-18 10:21:00 +0800

    dsdsad

---
+++ lab-activity-1/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String studentid = "20242283837";
+        String firstbaname = "Ron";
+        String lastname = "Deloso";
+        String course = "BSIT";
+        String section = "2-1";
+        int midterm = 89;
+        int finalexam = 87;
+        int ProjectS = 90;
+        int Attendance = 90;
+
+        int Total = midterm + finalexam + ProjectS + Attendance;
+        int AveScore = Total/4;
+
+        System.out.println("Student Name: " + firstbaname +lastname);
+        System.out.println("Student ID: " + studentid);
+        System.out.println("Student Name: " + firstbaname +lastname);
+        System.out.println("Course and section: " + course + section);
+        System.out.println(AveScore);
+        if (AveScore >= 75) {
+            System.out.println("PASSED!!!!");
+        }
+        else{
+            System.out.println("FAILED");
+        }
+
+    }
+}
---
+++ prelims-exam/src/Student.java
@@ -0,0 +1,68 @@
+class Student {
+    String name = "";
+    int age = 0;
+    String course = "";
+    double grade1 = 0;
+    double grade2 = 0;
+    double grade3 = 0;
+
+    double average = 0;
+    boolean isPassing = false;
+
+
+    public Student(String Studentname, int studentage, String Studentcourse, double Sgrade1, double Sgrade2, double Sgrade3, double Gaverage) {
+
+        name = Studentname;
+        age = studentage;
+        course = Studentcourse;
+        grade1 = Sgrade1;
+        grade2 = Sgrade2;
+        grade3 = Sgrade3;
+        average = Gaverage;
+
+        System.out.println("Student Information: ");
+    }
+
+    public void displayinfo() {
+        System.out.println("name: " + name);
+        System.out.println("age: " + age);
+        System.out.println("course: " + course);
+        System.out.println("Grades: " + grade1 + grade2 + grade3);
+    }
+
+    public void calculateAverage() {
+        average = (grade1 + grade2 + grade3) / 3;
+    }
+
+    public void getLetterGrade() {
+        if (average >= 90) {
+            System.out.println("letter grade: A");
+        } else if (average >= 80) {
+            System.out.println("letter grade: B");
+        } else if (average >= 70) {
+            System.out.println("letter grade: C");
+        } else if (average >= 60) {
+            System.out.println("letter grade: D");
+        } else if (average < 60) {
+            System.out.println("letter grade: F");
+        }
+    }
+
+    public void isPassing() {
+        if (average >= 70) {
+            System.out.println("Status: PASSING");
+        } else {
+            System.out.println("Status: FAILED");
+        }
+    }
+}
+class Classmain {
+    public static void main(String[] args) {
+        System.out.println();
+        Student student1 = new Student("Ron", 20);
+
+    }
+}
+
+
+
