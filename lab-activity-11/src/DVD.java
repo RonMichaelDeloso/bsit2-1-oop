9f6c1d011d17fac1f7c8757cff1130acf5dc3db8
RonMichaelDeloso <rdeloso12002@liceo.edu.ph>
2025-09-23 23:40:13 +0800

    Create DVD.java

---
+++ lab-activity-11/src/DVD.java
@@ -0,0 +1,36 @@
+class DVD extends Media {
+    private String director;
+
+    public DVD(String title, String itemId, String director) {
+        super(title, itemId);
+        this.director = director;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "DVD";
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("DVD: \"" + title + "\" directed by " + director + " (ID: " + itemId + ")");
+    }
+}
+class Magazine extends Media {
+    private int issueNumber;
+
+    public Magazine(String title, String itemId, int issueNumber) {
+        super(title, itemId);
+        this.issueNumber = issueNumber;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "Magazine";
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Magazine: \"" + title + "\" Issue #" + issueNumber + " (ID: " + itemId + ")");
+    }
+}
